589b9705f18ff6a773d41c6b8c51fd5375b6b0e4
Nguyen Duc Chung <chungnd10@gmail.com>
2019-10-25 23:23:59 +0700

    giao dien admin

---
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -0,0 +1,199 @@
+<aside class="main-sidebar">
+    <!-- sidebar: style can be found in sidebar.less -->
+    <section class="sidebar">
+        <ul class="sidebar-menu" data-widget="tree">
+            <li class="treeview">
+                <a href="#">
+                    <i class="fa fa-dashboard"></i> <span>Bảng tin</span>
+                    <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+                </a>
+                <ul class="treeview-menu">
+                    <li><a href=""><i class="fa fa-angle-right"></i> Dashboard v1</a></li>
+                </ul>
+            </li>
+            <li class="treeview">
+                <a href="#">
+                    <i class="fa fa-suitcase"></i> <span>Chi nhánh</span>
+                    <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+                </a>
+                <ul class="treeview-menu">
+                    <li><a href=""><i class="fa fa-angle-right"></i>Danh sách</a></li>
+                    <li><a href=""><i class="fa fa-angle-right"></i>Thêm mới</a></li>
+                </ul>
+            </li>
+            <li class="treeview">
+                <a href="#">
+                    <i class="fa fa-cubes"></i> <span>Loại dịch vụ</span>
+                    <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+                </a>
+                <ul class="treeview-menu">
+                    <li><a href=""><i class="fa fa-angle-right"></i>Danh sách</a></li>
+                    <li><a href=""><i class="fa fa-angle-right"></i>Thêm mới</a></li>
+                </ul>
+            </li>
+            <li class="treeview">
+                <a href="#">
+                    <i class="fa fa-cube"></i> <span>Dịch vụ</span>
+                    <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+                </a>
+                <ul class="treeview-menu">
+                    <li><a href=""><i class="fa fa-angle-right"></i>Danh sách</a></li>
+                    <li><a href=""><i class="fa fa-angle-right"></i>Thêm mới</a></li>
+                </ul>
+            </li>
+            <li class="treeview">
+                <a href="#">
+                    <i class="fa fa-book"></i> <span>Quy trình loại dịch vụ</span>
+                    <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+                </a>
+                <ul class="treeview-menu">
+                    <li><a href=""><i class="fa fa-angle-right"></i>Danh sách</a></li>
+                    <li><a href=""><i class="fa fa-angle-right"></i>Thêm mới</a></li>
+                </ul>
+            </li>
+            <li class="treeview">
+                <a href="#">
+                    <i class="fa fa-dollar"></i> <span>Hóa đơn</span>
+                    <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+                </a>
+                <ul class="treeview-menu">
+                    <li><a href=""><i class="fa fa-angle-right"></i>Danh sách</a></li>
+                    <li><a href=""><i class="fa fa-angle-right"></i>Thêm mới</a></li>
+                </ul>
+            </li>
+            <li class="treeview">
+                <a href="#">
+                    <i class="fa fa-dashboard"></i> <span>Lịch đặt</span>
+                    <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+                </a>
+                <ul class="treeview-menu">
+                    <li><a href=""><i class="fa fa-angle-right"></i>Danh sách</a></li>
+                    <li><a href=""><i class="fa fa-angle-right"></i>Thêm mới</a></li>
+                </ul>
+            </li>
+            <li class="treeview">
+                <a href="#">
+                    <i class="fa fa-dashboard"></i> <span>Tích điểm</span>
+                    <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+                </a>
+                <ul class="treeview-menu">
+                    <li><a href=""><i class="fa fa-angle-right"></i>Danh sách</a></li>
+                    <li><a href=""><i class="fa fa-angle-right"></i>Thêm mới</a></li>
+                </ul>
+            </li>
+            <li class="treeview">
+                <a href="#">
+                    <i class="fa fa-cut"></i> <span>Chiết khấu</span>
+                    <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+                </a>
+                <ul class="treeview-menu">
+                    <li><a href=""><i class="fa fa-angle-right"></i>Danh sách</a></li>
+                    <li><a href=""><i class="fa fa-angle-right"></i>Thêm mới</a></li>
+                </ul>
+            </li>
+            <li class="treeview">
+                <a href="#">
+                    <i class="fa fa-exclamation-triangle"></i> <span>Danh sách hạn chế</span>
+                    <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+                </a>
+                <ul class="treeview-menu">
+                    <li><a href=""><i class="fa fa-angle-right"></i>Danh sách</a></li>
+                    <li><a href=""><i class="fa fa-angle-right"></i>Thêm mới</a></li>
+                </ul>
+            </li>
+            <li class="treeview">
+                <a href="#">
+                    <i class="fa fa-users"></i> <span>Nhân viên</span>
+                    <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+                </a>
+                <ul class="treeview-menu">
+                    <li><a href=""><i class="fa fa-angle-right"></i>Danh sách</a></li>
+                    <li><a href=""><i class="fa fa-angle-right"></i>Thêm mới</a></li>
+                </ul>
+            </li>
+            <li class="treeview">
+                <a href="#">
+                    <i class="fa fa-user"></i> <span>Phân quyền</span>
+                    <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+                </a>
+                <ul class="treeview-menu">
+                    <li><a href=""><i class="fa fa-angle-right"></i>Danh sách</a></li>
+                    <li><a href=""><i class="fa fa-angle-right"></i>Thêm mới</a></li>
+                </ul>
+            </li>
+            <li class="treeview">
+                <a href="#">
+                    <i class="fa fa-envelope"></i> <span>Phản hồi</span>
+                    <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+                </a>
+                <ul class="treeview-menu">
+                    <li><a href=""><i class="fa fa-angle-right"></i>Danh sách</a></li>
+                    <li><a href=""><i class="fa fa-angle-right"></i>Thêm mới</a></li>
+                </ul>
+            </li>
+            <li class="treeview">
+                <a href="#">
+                    <i class="fa fa-tv"></i> <span>Trang giới thiệu</span>
+                    <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+                </a>
+                <ul class="treeview-menu">
+                    <li><a href=""><i class="fa fa-angle-right"></i>Danh sách</a></li>
+                    <li><a href=""><i class="fa fa-angle-right"></i>Thêm mới</a></li>
+                </ul>
+            </li>
+            <li class="treeview">
+                <a href="#">
+                    <i class="fa fa-file-image-o"></i> <span>Slides</span>
+                    <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+                </a>
+                <ul class="treeview-menu">
+                    <li><a href=""><i class="fa fa-angle-right"></i>Danh sách</a></li>
+                    <li><a href=""><i class="fa fa-angle-right"></i>Thêm mới</a></li>
+                </ul>
+            </li>
+            <li class="treeview">
+                <a href="#">
+                    <i class="fa fa-cog"></i> <span>Thông tin website</span>
+                    <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+                </a>
+                <ul class="treeview-menu">
+                    <li><a href=""><i class="fa fa-angle-right"></i>Danh sách</a></li>
+                    <li><a href=""><i class="fa fa-angle-right"></i>Thêm mới</a></li>
+                </ul>
+            </li>
+        </ul>
+    </section>
+    <!-- /.sidebar -->
+</aside>
