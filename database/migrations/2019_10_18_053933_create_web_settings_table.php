2a448c0bf20a45a8c3407c0c651b7b42af059b77
Nguyen Duc Chung <chungnd10@gmail.com>
2019-10-31 05:39:32 +0700

    websettings

--- database/migrations/2019_10_18_053933_create_web_settings_table.php
+++ database/migrations/2019_10_18_053933_create_web_settings_table.php
@@ -14,13 +14,14 @@ class CreateWebSettingsTable extends Migration
     public function up()
     {
         Schema::create('web_settings', function (Blueprint $table) {
+            $table->increments('id');
             $table->string('open_time', 10);
             $table->string('close_time', 10);
-            $table->string('facebook', 300);
-            $table->string('website_url', 200);
+            $table->string('facebook', 300)->nullable();
             $table->string('phone_number', 11);
             $table->string('email', 200);
-            $table->string('logo',200);
+            $table->string('logo',200)->default('logo-default.png');
+            $table->timestamps();
         });
     }
 
