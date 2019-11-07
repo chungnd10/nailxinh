0944d0b0577d34dbe00f37ea186b5e1609a3ec24
Nguyen Duc Chung <chungnd10@gmail.com>
2019-11-07 23:52:20 +0700

    fix bug

--- app/Services/WebSettingServices.php
+++ app/Services/WebSettingServices.php
@@ -6,6 +6,12 @@ use App\WebSetting;
 
 class WebSettingServices
 {
+    public function all()
+    {
+        $info = WebSetting::find(config('contants.web_settings'));
+        return $info;
+    }
+
     public function find($id)
     {
         $info = WebSetting::find($id);
